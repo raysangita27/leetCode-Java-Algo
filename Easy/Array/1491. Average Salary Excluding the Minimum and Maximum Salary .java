961a9ff7cd072f2f666e93ad9e93dfe359d12275
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-07 23:54:35 +0530

    Create 1491. Average Salary Excluding the Minimum and Maximum Salary .java

---
+++ Easy/Array/1491. Average Salary Excluding the Minimum and Maximum Salary .java
@@ -0,0 +1,87 @@
+/*
+Given an array of unique integers salary where salary[i] is the salary of the employee i.
+
+Return the average salary of employees excluding the minimum and maximum salary.
+
+ 
+
+Example 1:
+
+Input: salary = [4000,3000,1000,2000]
+Output: 2500.00000
+Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+Average salary excluding minimum and maximum salary is (2000+3000)/2= 2500
+Example 2:
+
+Input: salary = [1000,2000,3000]
+Output: 2000.00000
+Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+Average salary excluding minimum and maximum salary is (2000)/1= 2000
+Example 3:
+
+Input: salary = [6000,5000,4000,3000,2000,1000]
+Output: 3500.00000
+Example 4:
+
+Input: salary = [8000,9000,2000,3000,6000,1000]
+Output: 4750.00000
+ 
+
+Constraints:
+
+3 <= salary.length <= 100
+10^3 <= salary[i] <= 10^6
+salary[i] is unique.
+Answers within 10^-5 of the actual value will be accepted as correct.
+*/
+class Solution {
+    public double average(int[] salary) {
+        int len = salary.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i< len ; i++)
+        {
+            sum += salary[i];
+            max = Math.max(salary[i], max);
+            min = Math.min(salary[i], min);
+        }
+        return (double)(sum -(max+min))/(len-2);
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static String doubleToString(double input) {
+        return new DecimalFormat("0.00000").format(input);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] salary = stringToIntegerArray(line);
+            
+            double ret = new Solution().average(salary);
+            
+            String out = doubleToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
