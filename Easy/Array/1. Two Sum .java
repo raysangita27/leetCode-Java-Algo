3df12310c13f6ce7c7c482bb59ab36c659a77687
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-08 00:18:16 +0530

    Create 1. Two Sum .java

---
+++ Easy/Array/1. Two Sum .java
@@ -0,0 +1,82 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for(int i = 0; i< nums.length; i++)
+        {
+            if(map.containsKey(target - nums[i]) )
+            {
+                result[0] = map.get(target - nums[i]) ;
+                result[1] = i;
+                break;
+            }
+            else
+                map.put(nums[i], i);
+        }
+        return result;
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+    
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            line = in.readLine();
+            int target = Integer.parseInt(line);
+            
+            int[] ret = new Solution().twoSum(nums, target);
+            
+            String out = integerArrayToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
