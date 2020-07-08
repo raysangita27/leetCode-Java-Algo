aaafe495681cc278bd66d41a20400f00cb1ca747
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-08 23:48:06 +0530

    Create 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree .java

---
+++ Medium/Tree/1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree .java
@@ -0,0 +1,49 @@
+/*
+Given two binary trees original and cloned and given a reference to a node target in the original tree.
+
+The cloned tree is a copy of the original tree.
+
+Return a reference to the same node in the cloned tree.
+
+Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
+
+Follow up: Solve the problem if repeated values on the tree are allowed.
+
+ 
+
+Example 1:
+
+
+Input: tree = [7,4,3,null,null,6,19], target = 3
+Output: 3
+Explanation: In all examples the original and cloned trees are shown. The target node is a green node from the original tree. The answer is the yellow node from the cloned tree.
+Example 2:
+
+
+Input: tree = [7], target =  7
+Output: 7
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == null)
+            return null;
+        if(original.val == target.val)
+            return cloned;
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        if(left == null)
+            return getTargetCopy(original.right, cloned.right, target);
+        else
+            return left;
+    }
+}
