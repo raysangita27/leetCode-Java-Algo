d6fd2398c9d221bd3d20c3d11202d55a11a966f8
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-10 00:54:19 +0530

    Create 590. N-ary Tree Postorder Traversal .java

---
+++ Easy/Tree/590. N-ary Tree Postorder Traversal .java
@@ -0,0 +1,71 @@
+/*
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+
+ 
+
+Example 1:
+
+
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [5,6,3,2,4,1]
+Example 2:
+
+
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
+ 
+
+Constraints:
+
+The height of the n-ary tree is less than or equal to 1000
+The total number of nodes is between [0, 10^4]
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if(root == null)
+            return result;
+        Stack<Node> stk = new Stack<Node>();
+        Stack<Node> finalStack = new Stack<Node>();
+        stk.push(root);
+        while(!stk.isEmpty())
+        {
+            Node temp = stk.pop();
+            finalStack.push(temp);
+            for(int i = 0; i<temp.children.size(); i++)
+                stk.push(temp.children.get(i) );    
+        }
+        while(finalStack.size()>0 )
+            result.add(finalStack.pop().val);
+        return result;
+    }
+}
