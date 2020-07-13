d5bbfaacbd639502b5a58e2e00e8a1d5ed0216db
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-14 03:50:23 +0530

    Create 1441. Build an Array With Stack Operations .java

---
+++ Easy/Stack&Queue/1441. Build an Array With Stack Operations .java
@@ -0,0 +1,71 @@
+/*
+Given an array target and an integer n. In each iteration, you will read a number from  list = {1,2,3..., n}.
+
+Build the target array using the following operations:
+
+Push: Read a new element from the beginning list, and push it in the array.
+Pop: delete the last element of the array.
+If the target array is already built, stop reading more elements.
+You are guaranteed that the target array is strictly increasing, only containing numbers between 1 to n inclusive.
+
+Return the operations to build the target array.
+
+You are guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: 
+Read number 1 and automatically push in the array -> [1]
+Read number 2 and automatically push in the array then Pop it -> [1]
+Read number 3 and automatically push in the array -> [1,3]
+Example 2:
+
+Input: target = [1,2,3], n = 3
+Output: ["Push","Push","Push"]
+Example 3:
+
+Input: target = [1,2], n = 4
+Output: ["Push","Push"]
+Explanation: You only need to read the first 2 numbers and stop.
+Example 4:
+
+Input: target = [2,3,4], n = 4
+Output: ["Push","Pop","Push","Push","Push"]
+ 
+
+Constraints:
+
+1 <= target.length <= 100
+1 <= target[i] <= 100
+1 <= n <= 100
+target is strictly increasing.
+*/
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        int len = target.length;
+        int i = 1;
+        int k = 0;
+        List<String> result = new ArrayList<String>();
+        while(i <= n)
+        {   if(k == target.length)
+                break;
+            if( k < target.length && target[k] == i)
+            {
+                result.add("Push");
+                k++;
+            }
+            else
+            {
+                result.add("Push");
+                result.add("Pop");
+            }
+            i++;
+        }
+        return result;
+    }
+}
