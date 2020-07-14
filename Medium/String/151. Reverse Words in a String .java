9c7f5fd4a75699998ad69af4bc737ad4bebd84ec
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-15 01:47:50 +0530

    Create 151. Reverse Words in a String .java

---
+++ Medium/String/151. Reverse Words in a String .java
@@ -0,0 +1,44 @@
+/*
+Given an input string, reverse the string word by word.
+
+ 
+
+Example 1:
+
+Input: "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: "  hello world!  "
+Output: "world! hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Note:
+
+A word is defined as a sequence of non-space characters.
+Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+ */
+ class Solution {
+    public String reverseWords(String s) {
+        String[] word = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = word.length-1; i>=0; i--)
+        {
+            if(!word[i].isBlank())
+            {
+                sb.append(word[i]);
+                sb.append(" ");
+            }
+            
+        }
+         return sb.toString().trim();
+    }
+}
