c6b0976bb5765708540a04dcc106105a5686cbce
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-23 03:53:35 +0530

    Create 189. Rotate Array .java

---
+++ Easy/Array/189. Rotate Array .java
@@ -0,0 +1,59 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Follow up:
+
+Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 10^4
+It's guaranteed that nums[i] fits in a 32 bit-signed integer.
+k >= 0
+*/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        if(len == 0 || k == 0)
+            return;
+        if(k > len )
+            k = k%len;
+        k = len -k;
+        reverseArray(nums, 0, k-1);
+        reverseArray(nums, k, len-1);
+        reverseArray(nums, 0, len-1);
+    }
+    void reverseArray(int[] nums, int l, int r)
+    {
+        int i = l; 
+        int j = r;
+        while(i <= j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
