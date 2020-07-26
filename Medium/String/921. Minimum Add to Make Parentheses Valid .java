a686480f010a917139be0ee0868e1b15d0a6309a
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-27 01:28:54 +0530

    Create 921. Minimum Add to Make Parentheses Valid .java

---
+++ Medium/String/921. Minimum Add to Make Parentheses Valid .java
@@ -0,0 +1,64 @@
+/*
+Given a string S of '(' and ')' parentheses, we add the minimum number of parentheses ( '(' or ')', and in any positions ) so that the resulting parentheses string is valid.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+Given a parentheses string, return the minimum number of parentheses we must add to make the resulting string valid.
+
+ 
+
+Example 1:
+
+Input: "())"
+Output: 1
+Example 2:
+
+Input: "((("
+Output: 3
+Example 3:
+
+Input: "()"
+Output: 0
+Example 4:
+
+Input: "()))(("
+Output: 4
+ 
+
+Note:
+
+S.length <= 1000
+S only consists of '(' and ')' characters.
+
+*/
+
+class Solution {
+    public int minAddToMakeValid(String S) {
+        int len = S.length();
+        if(len == 0)
+            return 0; 
+        int count = 0;
+        Stack<Character> stk = new Stack<>();
+        for(char c : S.toCharArray())
+        {
+            if(c == ')')
+            {
+                if(!stk.isEmpty())
+                    stk.pop();
+                else
+                    count++;
+            }
+            else
+                stk.push(c);
+        }
+        while(!stk.isEmpty())
+        {
+            count++;
+            stk.pop();
+        }
+        return count;
+    }   
+}
