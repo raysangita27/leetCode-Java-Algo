8467b7a524e5195054b289295fd7fe36c3a80be0
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-31 12:06:02 +0530

    Create 986. Interval List Intersections .java

---
+++ Medium/Array/986. Interval List Intersections .java
@@ -0,0 +1,47 @@
+/*
+Given two lists of closed intervals, each list of intervals is pairwise disjoint and in sorted order.
+
+Return the intersection of these two interval lists.
+
+(Formally, a closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.  The intersection of two closed intervals is a set of real numbers that is either empty, or can be represented as a closed interval.  For example, the intersection of [1, 3] and [2, 4] is [2, 3].)
+
+ 
+
+![Example](https://github.com/raysangita27/leetCode-Java-Algo/blob/master/images/interval1.png)
+
+
+
+Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+ 
+
+Note:
+
+0 <= A.length < 1000
+0 <= B.length < 1000
+0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+*/
+class Solution {
+    public int[][] intervalIntersection(int[][] A, int[][] B) {
+        if(A.length==0 || B.length == 0)
+            return new int[][]{};
+        int m = A.length;
+        int n = B.length;
+        int i = 0;
+        int j = 0;
+        List<int[]> res = new ArrayList<>();
+        while(i < m && j < n)
+        {
+            int start = Math.max(A[i][0], B[j][0]);
+            int end = Math.min(A[i][1], B[j][1]);
+            if(start <= end)
+                res.add(new int[]{start,end});
+            if(A[i][1] < B[j][1])
+                i++;
+            else
+                j++;
+                
+        }
+        return res.toArray(new int[res.size()][2]);
+    }
+}
