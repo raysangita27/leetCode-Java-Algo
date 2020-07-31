48361cb82f505b276fc8a49b23d71f5f022b1420
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-08-01 01:43:47 +0530

    Update 510. Inorder Successor in BST II .java

--- Medium/Tree/510. Inorder Successor in BST II .java
+++ Medium/Tree/510. Inorder Successor in BST II .java
@@ -18,6 +18,39 @@ class Node {
 Follow up:
 
 Could you solve it without looking up any of the node's values?
+Example 1:
+
+
+Input: tree = [2,1,3], node = 1
+Output: 2
+Explanation: 1's in-order successor node is 2. Note that both the node and the return value is of Node type.
+![Example](https://github.com/raysangita27/leetCode-Java-Algo/blob/master/images/BSTInoSuc2.png)
+
+
+Input: tree = [5,3,6,2,4,null,null,1], node = 6
+Output: null
+Explanation: There is no in-order successor of the current node, so the answer is null.
+![Example](https://github.com/raysangita27/leetCode-Java-Algo/blob/master/images/BSTInoSuc3.png)
+
+
+Input: tree = [15,6,18,3,7,17,20,2,4,null,13,null,null,null,null,null,null,null,null,9], node = 15
+Output: 17
+Example 4:
+
+
+Input: tree = [15,6,18,3,7,17,20,2,4,null,13,null,null,null,null,null,null,null,null,9], node = 13
+Output: 15
+Example 5:
+
+Input: tree = [0], node = 0
+Output: null
+ 
+
+Constraints:
+
+-10^5 <= Node.val <= 10^5
+1 <= Number of Nodes <= 10^4
+All Nodes will have unique values.
 */
 
 /*
