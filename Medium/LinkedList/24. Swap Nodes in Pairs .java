4a6bacf86fe1b07e56efb833a7f4c27259c2aa1e
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-07-31 11:40:46 +0530

    Create 24. Swap Nodes in Pairs .java

---
+++ Medium/LinkedList/24. Swap Nodes in Pairs .java
@@ -0,0 +1,33 @@
+/*
+Given a linked list, swap every two adjacent nodes and return its head.
+
+You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+ 
+
+Example:
+
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+        firstNode.next = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+        return secondNode;
+    }
+}
