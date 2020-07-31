7105f2f37e4f4636341cfdfa3c628b0f291ae947
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-08-01 03:17:01 +0530

    Create 1038. Binary Search Tree to Greater Sum Tree .java

---
+++ Medium/Tree/1038. Binary Search Tree to Greater Sum Tree .java
@@ -0,0 +1,56 @@
+/*
+Given the root of a binary search tree with distinct values, modify it so that every node has a new value equal to the sum of the values of the original tree that are greater than or equal to node.val.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+![Example](https://github.com/raysangita27/leetCode-Java-Algo/blob/master/images/greaterSumTree.png)
+Input: [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+ 
+
+Constraints:
+
+The number of nodes in the tree is between 1 and 100.
+Each node will have value between 0 and 100.
+The given tree is a binary search tree.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    public TreeNode bstToGst(TreeNode root) {
+        if(root == null)
+            return null;
+        return generateSumTree(root);
+        
+    }
+    TreeNode generateSumTree(TreeNode root)
+    {
+        if(root == null)
+            return null;
+        generateSumTree(root.right);
+        sum += root.val;
+        root.val = sum;
+        generateSumTree(root.left);
+        return root;
+    }
+}
