9755ff8678b9b22751ff8a9562dd56f942229879
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-08-18 04:56:20 +0530

    Create 973. K Closest Points to Origin .java

---
+++ Medium/Array/973. K Closest Points to Origin .java
@@ -0,0 +1,51 @@
+/*
+We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+
+(Here, the distance between two points on a plane is the Euclidean distance.)
+
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+ 
+
+Example 1:
+
+Input: points = [[1,3],[-2,2]], K = 1
+Output: [[-2,2]]
+Explanation: 
+The distance between (1, 3) and the origin is sqrt(10).
+The distance between (-2, 2) and the origin is sqrt(8).
+Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
+Example 2:
+
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+ 
+
+Note:
+
+1 <= K <= points.length <= 10000
+-10000 < points[i][0] < 10000
+-10000 < points[i][1] < 10000
+*/
+
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        if(points.length == 0 || K >= points.length)
+            return points;
+        List<int[]> result = Arrays.asList(points);
+        Collections.sort(result, new Comparator<int[]>(){
+            public int compare(int[] pt1, int[] pt2)
+            {
+                double dist1 = Math.sqrt((pt1[0]*pt1[0])+ (pt1[1]*pt1[1]));
+                double dist2 = Math.sqrt((pt2[0]*pt2[0])+ (pt2[1]*pt2[1]));
+                return Double.compare(dist1, dist2);
+            }
+        });
+        int[][] closestPt = new int[K][2];
+        for(int i = 0; i<K; i++)
+            closestPt[i]  = result.get(i);
+        return closestPt;
+    }
+}
