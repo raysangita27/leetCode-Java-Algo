e60eb3d153677ed329820acfd624d73fcc8c3862
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2020-09-19 02:55:02 +0530

    Create 785. Is Graph Bipartite? .java

---
+++ Medium/Graph/785. Is Graph Bipartite? .java
@@ -0,0 +1,76 @@
+/*
+Given an undirected graph, return true if and only if it is bipartite.
+
+Recall that a graph is bipartite if we can split it's set of nodes into two independent subsets A and B such that every edge in the graph has one node in A and another node in B.
+
+The graph is given in the following form: graph[i] is a list of indexes j for which the edge between nodes i and j exists.  Each node is an integer between 0 and graph.length - 1.  There are no self edges or parallel edges: graph[i] does not contain i, and it doesn't contain any element twice.
+
+Example 1:
+Input: [[1,3], [0,2], [1,3], [0,2]]
+Output: true
+Explanation: 
+The graph looks like this:
+0----1
+|    |
+|    |
+3----2
+We can divide the vertices into two groups: {0, 2} and {1, 3}.
+Example 2:
+Input: [[1,2,3], [0,2], [0,1,3], [0,2]]
+Output: false
+Explanation: 
+The graph looks like this:
+0----1
+| \  |
+|  \ |
+3----2
+We cannot find a way to divide the set of nodes into two independent subsets.
+ 
+
+Note:
+
+graph will have length in range [1, 100].
+graph[i] will contain integers in range [0, graph.length - 1].
+graph[i] will not contain i or duplicate values.
+The graph is undirected: if any element j is in graph[i], then i will be in graph[j]
+*/
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int len = graph.length;
+        int[] colors = new int[len];
+        
+        for (int i = 0; i < len; i++) {
+            if (colors[i] != 0) continue;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.offer(i);
+            colors[i] = 1;   // Blue: 1; Red: -1.
+            
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                for (int next : graph[cur]) {
+                    if (colors[next] == 0) {          // If this node hasn't been colored;
+                        colors[next] = -colors[cur];  // Color it with a different color;
+                        queue.offer(next);
+                    } else if (colors[next] != -colors[cur]) {   // If it is colored and its color is different, return false;
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+/**************************Explanation *******************************************/
+
+/*
+Our goal is trying to use two colors to color the graph and see if there are any adjacent nodes having the same color.
+Initialize a color[] array for each node. Here are three states for colors[] array:
+0: Haven't been colored yet.
+1: Blue.
+-1: Red.
+For each node,
+
+If it hasn't been colored, use a color to color it. Then use the other color to color all its adjacent nodes (DFS).
+If it has been colored, check if the current color is the same as the color that is going to be used to color it.
+*/
