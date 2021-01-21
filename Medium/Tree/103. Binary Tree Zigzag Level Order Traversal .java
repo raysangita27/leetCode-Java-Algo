a4e3f1f08f375687cf20ecbc106b1c4e47a03c6e
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2021-01-22 02:03:16 +0530

    Update 103. Binary Tree Zigzag Level Order Traversal .java

--- Medium/Tree/103. Binary Tree Zigzag Level Order Traversal .java
+++ Medium/Tree/103. Binary Tree Zigzag Level Order Traversal .java
@@ -79,3 +79,38 @@ class Solution {
         return result;
     }
 }
+
+/****************using single queue ***********************/
+ public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+     List<List<Integer>> result = new ArrayList<List<Integer>>();
+    if (root == null)
+      return result;
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+    boolean leftToRight = true;
+    while (!queue.isEmpty()) {
+      int levelSize = queue.size();
+      List<Integer> currentLevel = new LinkedList<>();
+      for (int i = 0; i < levelSize; i++) {
+        TreeNode currentNode = queue.poll();
+
+        // add the node to the current level based on the traverse direction
+        if (leftToRight)
+          currentLevel.add(currentNode.val);
+        else
+          currentLevel.add(0, currentNode.val);
+
+        // insert the children of current node in the queue
+        if (currentNode.left != null)
+          queue.offer(currentNode.left);
+        if (currentNode.right != null)
+          queue.offer(currentNode.right);
+      }
+      result.add(currentLevel);
+      // reverse the traversal direction
+      leftToRight = !leftToRight;
+    }
+
+    return result;
+ }
