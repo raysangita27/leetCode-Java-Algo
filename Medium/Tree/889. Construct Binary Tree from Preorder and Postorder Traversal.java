8cd27daebfab152081481f7e4b4eab53aef7d430
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2022-04-16 23:57:53 +0530

    Create 889. Construct Binary Tree from Preorder and Postorder Traversal.java

---
+++ Medium/Tree/889. Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -0,0 +1,61 @@
+/*
+Given two integer arrays, preorder and postorder where preorder is the preorder traversal of a binary tree of distinct values and postorder is the postorder traversal of the same tree, reconstruct and return the binary tree.
+
+If there exist multiple answers, you can return any of them.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [1,2,4,5,3,6,7], postorder = [4,5,2,6,7,3,1]
+Output: [1,2,3,4,5,6,7]
+Example 2:
+
+Input: preorder = [1], postorder = [1]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 30
+1 <= preorder[i] <= preorder.length
+All the values of preorder are unique.
+postorder.length == preorder.length
+1 <= postorder[i] <= postorder.length
+All the values of postorder are unique.
+It is guaranteed that preorder and postorder are the preorder traversal and postorder traversal of the same binary tree
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preInd = 0;
+    int postInd = 0;
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        TreeNode root = new TreeNode(preorder[preInd++]);
+        if(root.val != postorder[postInd])
+            root.left = constructFromPrePost(preorder, postorder);
+        if(root.val != postorder[postInd])
+            root.right = constructFromPrePost(preorder, postorder);
+        postInd++;
+    return root;
+    }
+
+}
+/*
+space complexity : O(height)
+time complexity : O(N)
+*/
