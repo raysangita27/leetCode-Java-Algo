199fb1ce70a61889bde6dee24b541e820cabb68d
raysangita27 <43432148+raysangita27@users.noreply.github.com>
2022-05-15 00:45:50 +0530

    Update 212. Word Search II .java

--- Hard/Array/212. Word Search II .java
+++ Hard/Array/212. Word Search II .java
@@ -25,40 +25,65 @@ All inputs are consist of lowercase letters a-z.
 The values of words are distinct.
 */
 
-public List<String> findWords(char[][] board, String[] words) {
-	List<String> res = new ArrayList<>();
-
-	for (String word : words) {
-		boolean[][] visited = new boolean[board.length][board[0].length];
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				if (board[i][j] == word.charAt(0) && exist(board, visited, i, j, word, 0)) {
-					if (!res.contains(word))
-						res.add(word);
-				}
-			}
-		}
-	}
-	return res;
+class Solution {
+    List<String> result;
+    public List<String> findWords(char[][] board, String[] words) {
+        result = new ArrayList<>();
+        TrieNode root = new TrieNode();
+        if(board == null || board.length == 0)
+            return result;
+        for(String word : words)
+        {
+            insert(word, root);
+        }
+        for(int i = 0; i < board.length; i++)
+        {
+            for(int j = 0; j < board[0].length; j++)
+            {
+                dfs(board, i, j, root);
+            }
+        }
+        return result;
+    }
+    public void dfs(char[][] board, int i, int j , TrieNode node)
+    {
+        char c = board[i][j];
+        if(c == '#' || node.link[c - 'a'] == null)
+            return;
+        node = node.link[c-'a'];
+        if(node.word != null)
+        {
+            result.add(node.word);
+            node.word =null;
+        }
+        board[i][j] = '#';
+        if(i > 0)
+            dfs(board, i -1, j, node);
+        if(j > 0)
+            dfs(board, i, j-1, node);
+        if(i < board.length-1)
+            dfs(board, i+1, j, node);
+        if(j < board[0].length-1)
+            dfs(board, i, j+1, node);
+        
+        board[i][j] = c;
+        
+        
+    }
+    public void insert(String word, TrieNode node)
+    {
+        for(int i = 0; i < word.length(); i++)
+        {
+            char c = word.charAt(i);
+            if(node.link[c - 'a'] == null)
+                node.link[c - 'a'] = new TrieNode();
+            node = node.link[c - 'a'];
+        }
+        node.word = word;
+    }
 }
-
-private boolean exist(char[][] board, boolean[][] visited, int i, int j, String s, int index) {
-	if (index == s.length())
-		return true;
-	//false case
-	if (i < 0 || i >= board.length || j < 0 || 
-		j >= board[0].length || visited[i][j] || board[i][j] != s.charAt(index)) {
-		return false;
-	}
-
-	visited[i][j] = true;
-	if (exist(board, visited, i + 1, j, s, index + 1) ||
-		exist(board, visited, i - 1, j, s, index + 1) ||
-		exist(board, visited, i, j + 1, s, index + 1) ||
-		exist(board, visited, i, j - 1, s, index + 1)) {
-		return true;
-	}
-	visited[i][j] = false;
-
-	return false;
+class TrieNode{
+    TrieNode[] link = new TrieNode[26];
+    String word;
+    TrieNode(){};
 }
