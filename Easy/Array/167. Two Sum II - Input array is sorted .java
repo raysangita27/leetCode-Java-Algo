4e9d70deea7660954fbbe9acdbbd767a924f81d4
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-10 00:13:36 +0530

    Update 167. Two Sum II - Input array is sorted .java

--- Easy/Array/167. Two Sum II - Input array is sorted .java
+++ Easy/Array/167. Two Sum II - Input array is sorted .java
@@ -12,7 +12,6 @@ Example:
 Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
-*/
 Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
@@ -26,3 +25,25 @@ Example:
 Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+*/
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        if(numbers == null || numbers.length == 0)
+            return new int[]{-1,-1};
+        int len = numbers.length;
+        int l = 0;
+        int r = len-1;
+        while(l < r)
+        {
+            int sum = numbers[l] + numbers[r];
+            if(sum == target)
+                return new int[]{l+1, r+1};
+            else if(sum > target)
+                r--;
+            else
+                l++;
+        }
+        return new int[]{-1, -1};
+    }
+    
+}
