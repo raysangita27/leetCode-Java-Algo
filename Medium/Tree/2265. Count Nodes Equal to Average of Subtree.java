6dbebc2ecba5e3894c286993e149562b50c821c9
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-10 21:37:09 +0530

    Create 2265. Count Nodes Equal to Average of Subtree.java

---
+++ Medium/Tree/2265. Count Nodes Equal to Average of Subtree.java
@@ -0,0 +1,71 @@
+/*
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+ 
+
+Example 1:
+
+
+Input: root = [4,8,5,0,1,null,6]
+Output: 5
+Explanation: 
+For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+For the node with value 0: The average of its subtree is 0 / 1 = 0.
+For the node with value 1: The average of its subtree is 1 / 1 = 1.
+For the node with value 6: The average of its subtree is 6 / 1 = 6.
+Example 2:
+
+
+Input: root = [1]
+Output: 1
+Explanation: For the node with value 1: The average of its subtree is 1 / 1 = 1.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 1000
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int nodecount = 0;
+    int ans  = 0;
+    public int averageOfSubtree(TreeNode root) {
+        if(root == null)
+            return 0;
+        postorder(root);
+        return ans;
+        
+    }
+    public int[] postorder(TreeNode root){
+        if(root == null)
+            return new int[]{0,0};
+        int[] lVal = postorder(root.left);
+        int[] rVal = postorder(root.right);
+        int nodeCount = lVal[1] + rVal[1] +1 ;
+        int sum = lVal[0] + rVal[0] + root.val;
+        if(root.val == (sum/nodeCount))
+            ans++;
+        return new int[]{sum, nodeCount};
+    }
+}
