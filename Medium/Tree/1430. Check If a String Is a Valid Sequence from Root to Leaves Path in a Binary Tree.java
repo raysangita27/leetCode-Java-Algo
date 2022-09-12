0e5cebf4e624548ded1c5788fa963dbcbfd72302
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-13 00:44:53 +0530

    Create 1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java

---
+++ Medium/Tree/1430. Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java
@@ -0,0 +1,71 @@
+/*
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+Example 2:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+Example 3:
+
+
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,1]
+Output: false
+Explanation: The path 0 -> 1 -> 1 is a sequence, but it is not a valid sequence.
+ 
+
+Constraints:
+
+1 <= arr.length <= 5000
+0 <= arr[i] <= 9
+Each node's value is between [0 - 9].
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidSequence(TreeNode root, int[] arr) {
+        if(root == null)
+            return false;
+        return inorder(root, arr, 0);
+    }
+    public boolean inorder(TreeNode root, int[] arr, int i)
+    {
+        if(root == null || i >= arr.length || arr[i] != root.val)
+            return false;
+        if(root.left == null && root.right == null)
+            return i == arr.length-1;
+        return inorder(root.left, arr, i+1) || inorder(root.right, arr, i+1);
+    }
+}
