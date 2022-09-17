6f3990784f5d22fb457e131ab0d06c4adef31381
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-17 20:28:02 +0530

    Create 2363. Merge Similar Items.java

---
+++ Easy/Array/2363. Merge Similar Items.java
@@ -0,0 +1,85 @@
+/*
+You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
+
+items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+The value of each item in items is unique.
+Return a 2D integer array ret where ret[i] = [valuei, weighti], with weighti being the sum of weights of all items with value valuei.
+
+Note: ret should be returned in ascending order by value.
+
+ 
+
+Example 1:
+
+Input: items1 = [[1,1],[4,5],[3,8]], items2 = [[3,1],[1,5]]
+Output: [[1,6],[3,9],[4,5]]
+Explanation: 
+The item with value = 1 occurs in items1 with weight = 1 and in items2 with weight = 5, total weight = 1 + 5 = 6.
+The item with value = 3 occurs in items1 with weight = 8 and in items2 with weight = 1, total weight = 8 + 1 = 9.
+The item with value = 4 occurs in items1 with weight = 5, total weight = 5.  
+Therefore, we return [[1,6],[3,9],[4,5]].
+Example 2:
+
+Input: items1 = [[1,1],[3,2],[2,3]], items2 = [[2,1],[3,2],[1,3]]
+Output: [[1,4],[2,4],[3,4]]
+Explanation: 
+The item with value = 1 occurs in items1 with weight = 1 and in items2 with weight = 3, total weight = 1 + 3 = 4.
+The item with value = 2 occurs in items1 with weight = 3 and in items2 with weight = 1, total weight = 3 + 1 = 4.
+The item with value = 3 occurs in items1 with weight = 2 and in items2 with weight = 2, total weight = 2 + 2 = 4.
+Therefore, we return [[1,4],[2,4],[3,4]].
+Example 3:
+
+Input: items1 = [[1,3],[2,2]], items2 = [[7,1],[2,2],[1,4]]
+Output: [[1,7],[2,4],[7,1]]
+Explanation:
+The item with value = 1 occurs in items1 with weight = 3 and in items2 with weight = 4, total weight = 3 + 4 = 7. 
+The item with value = 2 occurs in items1 with weight = 2 and in items2 with weight = 2, total weight = 2 + 2 = 4. 
+The item with value = 7 occurs in items2 with weight = 1, total weight = 1.
+Therefore, we return [[1,7],[2,4],[7,1]].
+ 
+
+Constraints:
+
+1 <= items1.length, items2.length <= 1000
+items1[i].length == items2[i].length == 2
+1 <= valuei, weighti <= 1000
+Each valuei in items1 is unique.
+Each valuei in items2 is unique.
+
+*/
+
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] item1, int[][] item2) {
+        List<List<Integer>>  result = new ArrayList<>();
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int i = 0;
+        int j = 0;
+        while(i < item1.length || j < item2.length)
+        {
+            if(i < item1.length)
+            {
+                if(!map.containsKey(item1[i][0]))
+                    map.put(item1[i][0], item1[i][1]);
+                else
+                    map.put(item1[i][0], map.get(item1[i][0])+ item1[i][1]);
+                i++;
+            }
+            if(j < item2.length)
+            {
+                if(!map.containsKey(item2[j][0]))
+                    map.put(item2[j][0], item2[j][1]);
+                else
+                    map.put(item2[j][0], map.get(item2[j][0])+ item2[j][1]);
+                j++;
+            }
+        }
+        for(var key : map.keySet())
+        {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(key);
+            temp.add(map.get(key));
+            result.add(temp);
+        }
+        return result;
+    }
+}
