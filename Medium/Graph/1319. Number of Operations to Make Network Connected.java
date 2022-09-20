55c65abe65c046c7c28e0e131efb56b1d80f9d1d
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-21 01:09:01 +0530

    Create 1319. Number of Operations to Make Network Connected.java

---
+++ Medium/Graph/1319. Number of Operations to Make Network Connected.java
@@ -0,0 +1,82 @@
+/*
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+Example 2:
+
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+ 
+
+Constraints:
+
+1 <= n <= 105
+1 <= connections.length <= min(n * (n - 1) / 2, 105)
+connections[i].length == 2
+0 <= ai, bi < n
+ai != bi
+There are no repeated connections.
+No two computers are connected by more than one cable.
+*/
+
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        if(connections == null || connections.length == 0)
+            return -1;
+        if (connections.length < n - 1) return -1;
+        int[] parent = new int[n];
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+        int[] rank = new int[n];
+        Arrays.fill(rank, -1);
+        int totalCom = n;
+        int cycleCount = 0;
+        for(int[]e : connections)
+        {
+            int start = find(e[0], parent);
+            int end = find(e[1], parent);
+            if(start != end)
+                totalCom -= union(start, end , parent, rank);
+            else
+                cycleCount++;
+        }
+        return totalCom-1;
+    }
+    public int find(int i, int[] parent)
+    {
+        if(parent[i] == i)
+            return i;
+        return find(parent[i], parent);
+    }
+    public int union(int start, int end, int[] parent, int[] rank)
+    {
+        if(rank[start] > rank[end])
+        {
+            parent[end] = start;
+            rank[start] += rank[end];
+        }
+        else
+        {
+            parent[start] = end;
+            rank[end] += start;
+        }
+        return 1;
+    }
+}
