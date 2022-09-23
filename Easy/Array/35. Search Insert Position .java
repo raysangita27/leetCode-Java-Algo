ba1edd09afa5738d8b3bd3b4e378ab4b9eb7ea3e
sangita <43432148+raysangita27@users.noreply.github.com>
2022-09-24 02:48:50 +0530

    Update 35. Search Insert Position .java

--- Easy/Array/35. Search Insert Position .java
+++ Easy/Array/35. Search Insert Position .java
@@ -36,3 +36,27 @@ class Solution {
         return i;
     }
 }
+
+/**********************************************binary search**************************************************/
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int l = 0;
+        int r = nums.length-1;
+        while(l <= r)
+        {
+            int mid = (l + r)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(target< nums[mid])
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+        return l;
+    }
+}
+/*
+Timecomplexity => O(log(n))
+*/
