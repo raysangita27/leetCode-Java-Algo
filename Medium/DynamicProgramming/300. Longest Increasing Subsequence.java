1a68462db80aeacee522d16d37810b9e4ef23288
sangita <43432148+raysangita27@users.noreply.github.com>
2023-02-18 01:32:00 +0530

    Create 300. Longest Increasing Subsequence.java

---
+++ Medium/DynamicProgramming/300. Longest Increasing Subsequence.java
@@ -0,0 +1,54 @@
+/*
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence
+.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+ 
+
+Constraints:
+
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+*/
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+
+        for(int i = 1; i < nums.length; i++)
+        {
+            for(int j = 0; j < i; j ++ )
+            {
+                if(nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j] +1);
+            }
+        }
+        int maxLen = 0;
+        for(int n : dp)
+        {
+            maxLen = Math.max(n, maxLen);
+        }
+        return maxLen;
+    }
+}
