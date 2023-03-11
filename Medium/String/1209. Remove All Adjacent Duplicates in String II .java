6eb502d508dfc3dc7bdcffc12ee6ea10ea7c04b4
sangita <43432148+raysangita27@users.noreply.github.com>
2023-03-11 07:36:21 +0530

    Update 1209. Remove All Adjacent Duplicates in String II .java

--- Medium/String/1209. Remove All Adjacent Duplicates in String II .java
+++ Medium/String/1209. Remove All Adjacent Duplicates in String II .java
@@ -67,3 +67,29 @@ class Node{
         this.c = c;
     }
 }
+
+/****** using stringbuilder as stack and counter array to many frequency*****/
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        if(s == null || s.length() == 0)
+            return "";
+        
+        int[] count = new int[s.length()];
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray())
+        {
+            sb.append(c);
+            int last = sb.length()-1;
+            if(last > 0 && sb.charAt(last) == sb.charAt(last-1))
+                count[last] = count[last-1]+1;
+            else    
+                count[last] = 1;
+            if(count[last] >= k)
+                sb.delete(sb.length()-k, sb.length());
+        }
+
+        return sb.toString();
+    }
+}
