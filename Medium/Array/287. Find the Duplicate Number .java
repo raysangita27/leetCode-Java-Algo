812b03601992fe6b86fe64dae2bf80ca4d223b68
sangita <43432148+raysangita27@users.noreply.github.com>
2023-10-04 20:18:32 +0530

    Update 287. Find the Duplicate Number .java

--- Medium/Array/287. Find the Duplicate Number .java
+++ Medium/Array/287. Find the Duplicate Number .java
@@ -38,3 +38,25 @@ class Solution {
         return -1;
     }
 }
+
+/**************************************/
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int duplicate = -1;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.abs(nums[i]);
+            if (nums[cur] < 0) {
+                duplicate = cur;
+                break;
+            }
+            nums[cur] *= -1;
+        }
+        
+        // Restore numbers
+        for (int i = 0; i < nums.length; i++)
+            nums[i] = Math.abs(nums[i]);
+
+        return duplicate;
+    }
+
+}
