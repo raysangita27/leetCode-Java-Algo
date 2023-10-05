651a032cde45607039a03b8f7ab4b69424d0ca4f
sangita <43432148+raysangita27@users.noreply.github.com>
2023-10-05 23:46:53 +0530

    Update 14. Longest Common Prefix .java

--- Easy/String/14. Longest Common Prefix .java
+++ Easy/String/14. Longest Common Prefix .java
@@ -40,3 +40,17 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+/*******************************without sorting O(S) s= sum of length of all words**********************/
+public String longestCommonPrefix(String[] strs) {
+    if (strs == null || strs.length == 0) return "";
+    for (int i = 0; i < strs[0].length() ; i++){
+        char c = strs[0].charAt(i);
+        for (int j = 1; j < strs.length; j ++) {
+            if (i == strs[j].length() || strs[j].charAt(i) != c)
+                return strs[0].substring(0, i);             
+        }
+    }
+    return strs[0];
+}
