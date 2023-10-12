b000e34c8c030bd3ba7db56166f803f1fa4cd94a
sangita <43432148+raysangita27@users.noreply.github.com>
2023-10-12 13:15:29 +0530

    Create 1099. Two Sum Less Than K.java

---
+++ Easy/Array/1099. Two Sum Less Than K.java
@@ -0,0 +1,48 @@
+/*
+Given an array nums of integers and integer k, return the maximum sum such that there exists i < j with nums[i] + nums[j] = sum and sum < k. If no i, j exist satisfying this equation, return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [34,23,1,24,75,33,54,8], k = 60
+Output: 58
+Explanation: We can use 34 and 24 to sum 58 which is less than 60.
+Example 2:
+
+Input: nums = [10,20,30], k = 15
+Output: -1
+Explanation: In this case it is not possible to get a pair sum less that 15.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 1000
+1 <= k <= 2000
+*/
+
+class Solution {
+    public int twoSumLessThanK(int[] nums, int k) {
+        if(nums == null || nums.length < 2)
+            return -1;
+
+        int sum = 0;
+        Arrays.sort(nums);
+        int l = 0;
+        int r = nums.length-1;
+        boolean exist = false;
+        while(l < r)
+        {
+            int temp = nums[l] + nums[r];
+            if(temp < k){
+                exist = true;
+                sum = Math.max(sum , temp);
+                l++;
+            }
+            else
+                r--;
+        }
+        return !exist ? -1 : sum;
+    }
+}
