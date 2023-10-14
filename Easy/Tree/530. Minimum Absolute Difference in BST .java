bd3c85d1748e7e576d87dfe921b69d59bf50e7a5
sangita <43432148+raysangita27@users.noreply.github.com>
2023-10-14 23:55:00 +0530

    Update 530. Minimum Absolute Difference in BST .java

--- Easy/Tree/530. Minimum Absolute Difference in BST .java
+++ Easy/Tree/530. Minimum Absolute Difference in BST .java
@@ -60,3 +60,41 @@ class Solution {
         inOrderList(root.right, list);
     }
 }
+
+/************************ recursive*******************/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int minDiff = Integer.MAX_VALUE;
+    TreeNode prev = null;
+    public int getMinimumDifference(TreeNode root) {
+        if(root == null)
+            return -1;
+        inorder(root);
+        return minDiff;
+    }
+    public void inorder(TreeNode root)
+    {
+        if(root == null)
+            return;
+        inorder(root.left);
+        if(prev != null)
+            minDiff = Math.min(minDiff, Math.abs( prev.val - root.val));
+        prev = root;
+        inorder(root.right);
+
+    }
+}
