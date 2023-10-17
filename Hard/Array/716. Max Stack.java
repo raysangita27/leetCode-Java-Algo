d0f9b459184fe406c6135488a66bfac5392a3d01
sangita <43432148+raysangita27@users.noreply.github.com>
2023-10-17 22:39:46 +0530

    Create 716. Max Stack.java

---
+++ Hard/Array/716. Max Stack.java
@@ -0,0 +1,102 @@
+/*
+Design a max stack data structure that supports the stack operations and supports finding the stack's maximum element.
+
+Implement the MaxStack class:
+
+MaxStack() Initializes the stack object.
+void push(int x) Pushes element x onto the stack.
+int pop() Removes the element on top of the stack and returns it.
+int top() Gets the element on the top of the stack without removing it.
+int peekMax() Retrieves the maximum element in the stack without removing it.
+int popMax() Retrieves the maximum element in the stack and removes it. If there is more than one maximum element, only remove the top-most one.
+You must come up with a solution that supports O(1) for each top call and O(logn) for each other call.
+
+ 
+
+Example 1:
+
+Input
+["MaxStack", "push", "push", "push", "top", "popMax", "top", "peekMax", "pop", "top"]
+[[], [5], [1], [5], [], [], [], [], [], []]
+Output
+[null, null, null, null, 5, 5, 1, 5, 1, 5]
+
+Explanation
+MaxStack stk = new MaxStack();
+stk.push(5);   // [5] the top of the stack and the maximum number is 5.
+stk.push(1);   // [5, 1] the top of the stack is 1, but the maximum is 5.
+stk.push(5);   // [5, 1, 5] the top of the stack is 5, which is also the maximum, because it is the top most one.
+stk.top();     // return 5, [5, 1, 5] the stack did not change.
+stk.popMax();  // return 5, [5, 1] the stack is changed now, and the top is different from the max.
+stk.top();     // return 1, [5, 1] the stack did not change.
+stk.peekMax(); // return 5, [5, 1] the stack did not change.
+stk.pop();     // return 1, [5] the top of the stack and the max element is now 5.
+stk.top();     // return 5, [5] the stack did not change.
+ 
+
+Constraints:
+
+-107 <= x <= 107
+At most 105 calls will be made to push, pop, top, peekMax, and popMax.
+There will be at least one element in the stack when pop, top, peekMax, or popMax is called.
+stack [1,5], [2,1],[3,5]
+maxStack [2,1] [1,5] [3,5]
+
+*/
+
+class MaxStack {
+    TreeSet<int[]> stack;
+    TreeSet<int[]> maxStack;
+    int index;
+    public MaxStack() {
+        Comparator comp = new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2)
+            {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0];    
+            }
+        };
+        stack = new TreeSet<>(comp);
+        
+        maxStack = new TreeSet<>(comp);
+        index = 1;
+        
+    }
+    
+    public void push(int x) {
+        stack.add(new int[]{index , x});
+        maxStack.add(new int[]{x, index});
+        index++;
+    }
+    
+    public int pop() {
+        var item = stack.pollLast() ;
+        maxStack.remove(new int[]{item[1], item[0]});
+        return item[1] ;  
+    }
+    
+    public int top() {
+         var item = stack.last() ;
+         return item[1];   
+    }
+    
+    public int peekMax() {
+        var item = maxStack.last() ;
+         return item[0];      
+    }
+    
+    public int popMax() {
+        var item = maxStack.pollLast() ;  
+        stack.remove(new int[]{item[1], item[0]});
+        return item[0];
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
