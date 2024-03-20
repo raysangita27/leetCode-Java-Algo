4d6889973d82e4f2232e0abe2cb48f05179d3524
sangita <43432148+raysangita27@users.noreply.github.com>
2024-03-21 01:58:23 +0530

    Update 921. Minimum Add to Make Parentheses Valid .java

--- Medium/String/921. Minimum Add to Make Parentheses Valid .java
+++ Medium/String/921. Minimum Add to Make Parentheses Valid .java
@@ -58,3 +58,29 @@ class Solution {
 }
 }
 }
+/*********************** implementation without stack*****************************/
+
+class Solution {
+    public int minAddToMakeValid(String s) {
+        if(s == null || s.length() == 0)
+            return 0;
+        int left = 0;
+        int right = 0;
+
+        for(char c : s.toCharArray())
+        {
+            if(c == '(')
+            {
+                left++;
+            }
+            else if( c == ')')
+            {
+                if(left > 0)
+                    left--;
+                else
+                    right++;
+            }
+        }    
+        return left + right;
+    }
+}
